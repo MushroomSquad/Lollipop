9f893d5faea24c6af47ce9df04bb257a1aca2d8d
MushroomSquad <donsudak@gmail.com>
2022-11-25 14:03:19 +0300

    removed trash commit

--- Templayter.build/__helpers.h
+++
@@ -1,6 +0,0 @@
-#ifndef __NUITKA_CALLS_H__
-#define __NUITKA_CALLS_H__
-
-extern PyObject *CALL_FUNCTION_WITH_ARGS12(PyObject *called, PyObject *const *args);
-extern PyObject *CALL_FUNCTION_WITH_ARGS15(PyObject *called, PyObject *const *args);
-#endif
